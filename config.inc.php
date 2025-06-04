8dd64b002a70530c5ddcfb855fc57ce7eca7995d
MMITSG Developer <sumeyokotun@gmail.com>
2025-06-04 10:48:43 +0630

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 $driver = "mysql";
 $host = "localhost";
 $port = 3306;
-$user = "";
-$pass = "";
-$db = "";
+$user = "mmitsg_dcp_25";
+$pass = "Fm3mjlZ^AOB&";
+$db = "mmitsg_dcp_25";
 $tpl = "default";
